aa7df1f37da7e3d80e7bce7a28f0130f0960db5a
wusd <wsd5656126@foxmail.com>
2020-11-15 21:45:54 +0800

    READEME.md 和 平方根

---
+++ README.md
@@ -0,0 +1,4 @@
+# SICP实践代码
+## 安装MIT-SCHEME
+apt install mit-scheme  
+mit-scheme
---
+++ src/sqrt.lisp
@@ -0,0 +1,25 @@
+# 亚历山大平方根
+(DEFINE SQRT (lambda (x) (TRY 1.0 x)))
+(DEFINE TRY (lambda (GUESS X)
+				(IF (GOOD-ENOUGHT? GUESS X)
+					(GUESS)
+					(TRY (IMPROVE GUESS X)
+						  X))))
+(DEFINE (GOOD-ENOUGHT? GUESS X)
+	(IF ((ABS
+			(- (SQUARE GUESS)
+				(SQUARE
+					(AVERAGE GUESS
+							(/ X GUESS))))))
+		true
+		false))
+(DEFINE (IMPROVE GUESS X)
+	(AVERAGE GUESS
+			(/ X GUESS)))
+(DEFINE ABS (lambda (x)
+				((COND ((< x 0) (- x))
+					  ((= x 0) 0)
+					  ((> x 0) x)))))
+(DEFINE AVERAGE (lambda (x y)
+	(/ (+ x y) 2)))
+(sqrt 17)
