194134520b4c1baaefd26aedc63e464c896b9c5f
root <wsd5656126@foxmail.com>
2021-04-19 00:28:33 +0800

    -

--- src/c01/ex1.06.scm
+++ src/c01/ex1.06.scm
@@ -2,6 +2,9 @@
 (define (new-if predicate then-clause else-clause)
   (cond (predicate then-clause)
         (else else-clause)))
+;;测试程序
+(new-if (= 2 3) 0 5)
+(new-if (= 0 0) 0 5)
 ;;new-if不可以
 (define (sqrt-iter guess x)
   (new-if (good-enough? guess x)
