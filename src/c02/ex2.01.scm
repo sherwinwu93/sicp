cc39f4c30436f772e0a91c1597c5b6bd824256ac
root <wsd5656126@foxmail.com>
2021-04-29 00:28:18 +0800

    code

---
+++ src/c02/ex2.01.scm
@@ -0,0 +1,16 @@
+(load "p57-make-rat.scm")
+;; 改进可以,处理正负问题
+(define (make-rat n d)
+  (let ((g (gcd n d))
+        (n-pos (> n 0))
+        (p-pos (> d 0)))
+    (cond ((and n-pos p-pos) (cons (/ n g) (/ d g)))
+          ((and n-pos (not p-pos)) (cons (- (/ n g)) (- (/ d g))))
+          ((and (not n-pos) p-pos) (cons (/ n g) (/ d g)))
+          ((and (not n-pos) (not p-pos)) (cons (- (/ n g)) (- (/ d g)))))))
+
+
+(print-rat (make-rat -1 1))
+(print-rat (make-rat 1 -1))
+(print-rat (make-rat -1 -1))
+(print-rat (make-rat 1 -1))
---
+++ src/c02/ex2.03.scm
@@ -0,0 +1,12 @@
+(load "ex2.02.scm")
+
+;; 矩形的表示: 计算周长和面积
+;; 第一种表示,用两条线段,选择:长宽
+(define (make-retangle a b)
+  (cons a b))
+(define (length-retangle r)
+  )
+(define (width-retangle r)
+  )
+
+;; 第二种表示,用ABC三个点构造,选择:长宽
