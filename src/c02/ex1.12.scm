530421afdf89952db002e9d280f7e2d336e09c22
root <wsd5656126@foxmail.com>
2021-05-05 23:49:52 +0800

    code

--- src/c02/ex1.12.scm
+++
@@ -1,7 +0,0 @@
-(load "ex1.11.scm")
-;; 再增加百分比
-(define (make-center-percent c p)
-  (let ((w (/ (* c p) 100.0)))
-    (make-center-width  c w)))
-(define (percent i)
-  (* 100 (/ (width i) (center i))))
