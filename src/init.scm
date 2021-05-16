c98356c313b68fecb5c4a15b8813c781f4b5a27c
wusd <wsd5656126@foxmail.com>
2021-05-16 17:20:27 +0800

    code

--- src/init.scm
+++ src/init.scm
@@ -1,4 +1,4 @@
-(define $sicp-dir "/root/git-code/sicp/src/")
+(define $sicp-dir "/home/wusd/git-code/sicp/src/")
 (define (absolute relative)
   (string-append $sicp-dir
                  relative))
