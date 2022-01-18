5f5a81a7384cad24c0064ee509c123e1993dc767
root <root@DESKTOP-HESRDSO.localdomain>
2022-01-18 23:44:49 +0800

    -

--- src/c01/ex1.46.scm
+++ src/c01/ex1.46.scm
@@ -1,3 +1,24 @@
 ;; 迭代式改进
 ;; iterative-improve, good-enough? improve, 返回lambda
 ;; 重新sqrt和fixed-point
+(load-r "lib/math.scm")
+(define (iterative-improve good-enough? improve)
+  (lambda(first-guess)
+    (define (try guess)
+      (if (good-enough? guess)
+	  guess
+	  (try (improve guess))))
+    (try first-guess)))
+
+
+(define (fixed-point f first-guess)
+  ((iterative-improve
+    (lambda(y) (< (abs (- y (f y)))
+		  0.00001))
+    (lambda(y) (f y)))
+   1.0))
+
+(define (sqrt x)
+  (fixed-point (lambda (y) (average y (/ x y))) 1.0))
+
+(sqrt 4.0)
